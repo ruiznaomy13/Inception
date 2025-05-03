21df870420be8388bbe1c9fd7222321a390d9058
ruiznaomy13 <ruiznaomy13@gmail.com>
2025-05-04 00:39:05 +0200

    openssl

--- srcs/requirements/nginx/tools/entrypoint.sh
+++ srcs/requirements/nginx/tools/entrypoint.sh
@@ -5,6 +5,24 @@ if ! touch /etc/nginx/nginx.conf 2>/dev/null; then
     exit 1
 fi
 
+mkdir -p /etc/nginx/ssl
+
+openssl req -x509 -nodes -days 365 \
+		-newkey rsa:2048 \
+		-keyout /etc/nginx/ssl/ncastell.key \
+		-out /etc/nginx/ssl/ncastell.crt \
+		-subj "/C=ES/ST=Catalunia/L=Barcelona/O=42Barcelona/OU=Fundacion Telefonica/CN=ncastell.42.fr"
+
+sed -i '/http {/a \
+server {\
+	listen 443 ssl;\
+	server_name ncastell.42.fr;\
+	ssl_certificate /etc/nginx/ssl/ncastell.crt;\
+	ssl_certificate_key /etc/nginx/ssl/ncastell.key;\
+	ssl_protocols TLSv1.2 TLSv1.3;\
+}' /etc/nginx/nginx.conf
+
+
 echo "NGINX configuration correctly! Ready to run up! :)"
 
 exec "$@"
