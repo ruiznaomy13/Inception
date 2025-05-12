2935b9a73282429d3d61d006900ef469d9b4f18c
ruiznaomy13 <ruiznaomy13@gmail.com>
2025-05-12 16:04:16 +0200

    finished

---
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+if [ "$1" = 'php-fpm82' ]; then
+    sed -i 's|^listen = .*|listen = 9000|' /etc/php82/php-fpm.d/www.conf
+	if [ ! -f "wp-config.php" ]; then
+		echo "WordPress: Configuring WordPress..."
+
+		curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+		chmod +x wp-cli.phar
+
+		WP="php82 -d memory_limit=256M wp-cli.phar"
+
+		$WP core download --allow-root
+
+		$WP config create \
+			--dbname="$WORDPRESS_DB_NAME" \
+			--dbuser="$WORDPRESS_DB_USER" \
+			--dbpass="$WORDPRESS_DB_PASSWORD" \
+			--dbhost="$WORDPRESS_DB_HOST"
+
+
+		$WP core install \
+			--title="Inception" \
+			--url="$WORDPRESS_URL" \
+			--admin_user="$WORDPRESS_DB_USER" \
+			--admin_password="$WORDPRESS_DB_PASSWORD" \
+			--admin_email="$WORDPRESS_DB_USER@example.com"
+
+		chown -R www-data:www-data .
+	fi
+
+
+    echo "WordPress: Config initialized."
+fi
+
+exec "$@"
